4d45c0690e1bed0ef87b34bca41a9d2d0d0eafb7
Salome Gomez <sgomez713@misena.edu.co>
2022-03-23 20:55:44 -0500

    se creo el servidor con express y se creo la primera ruta

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRoute = require('./routes/user.routes')
+
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL
+    )
+    .then(() => console.log("DB Connection Successfull!"))
+    .catch((err) => {
+        console.log(err)
+    });
+
+app.use("/api/user", userRoute);
+
+app.listen(process.env.PORT || 3002, () => {
+    console.log("Backend server is running!")
+});
