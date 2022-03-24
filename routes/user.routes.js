bb90af9d9c87e38b4dc99582058d55e441676995
Salome Gomez <sgomez713@misena.edu.co>
2022-03-23 20:59:08 -0500

    se creo la segunda ruta

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,5 +5,10 @@ router.get("/usertest", (req, res) => {
     res.send("user test is successful")
 });
 
+router.post("userposttest", (req, res) => {
+    const username = req.body.username;
+    console.log(username);
+})
+
 
 module.exports = router
