b9a1f84d53c0b4df494e62ade2509c284b1d52e0
Salome Gomez <sgomez713@misena.edu.co>
2022-03-25 20:10:38 -0500

    se encripto los passwords de register con crypto-js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const userRoute = require('./routes/user.routes')
+const userRoute = require('./routes/user.routes');
+const authRoute = require('./routes/auth.routes');
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ mongoose.connect(
 
 app.use(express.json());
 
+app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 3002, () => {
