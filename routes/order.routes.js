bfb134e3ce685a9e1c1a1c116cbacae2e366ec66
Salome Gomez <sgomez713@misena.edu.co>
2022-04-08 21:18:15 -0500

    se creo logica para buscar todas las ordenes

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -56,15 +56,15 @@ router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
 });
 
 
-// //GET ALL
-// router.get("/", verifyTokenAndAdmin, async (req, res) => {
-//     try {
-//         const carts = await Cart.find();
-//         res.status(200).json(carts);
-//     } catch (err) {
-//         res.status(500).json(err)
-//     }
-// });
+//GET ALL
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const orders = await Order.find();
+        res.status(200).json(orders);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
 
 
 
