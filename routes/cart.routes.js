d6721104fd150065c9ab9747653e7c0749a9e5b1
Salome Gomez <sgomez713@misena.edu.co>
2022-04-08 20:52:51 -0500

    se creo logica para la funcion de eliminar carrito

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -33,15 +33,15 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
 });
 
 
-// //DELETE
-// router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
-//     try {
-//         await Product.findByIdAndDelete(req.params.id)
-//         res.status(200).json("Product has been deleted")
-//     } catch (err) {
-//         res.status(500).json(err)
-//     }
-// });
+//DELETE
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        await Cart.findByIdAndDelete(req.params.id)
+        res.status(200).json("Cart has been deleted")
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
 
 
 // //GET PRODUCT
