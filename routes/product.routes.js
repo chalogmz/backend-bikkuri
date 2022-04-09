0d6d8416d1f4331bdbf4922691ef8f2aa1608ca2
Salome Gomez <sgomez713@misena.edu.co>
2022-04-08 20:42:08 -0500

    se creo logica para las funciones de buscar el ultimo producto creado, buscar por categoria y buscar todos los productos

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -56,49 +56,31 @@ router.get("/find/:id", async (req, res) => {
 });
 
 
-// //GET ALL USERS
-// router.get("/", verifyTokenAndAdmin, async (req, res) => {
-//     const query = req.query.new
+//GET ALL USERS
+router.get("/", async (req, res) => {
+    const qNew = req.query.new;
+    const qCategory = req.query.category;
 
-//     try {
-//         const users = query 
-//             ? await User.find().sort({_id:-1}).limit(5) 
-//             : await User.find();
-//         res.status(200).json(users);
-//     } catch (err) {
-//         res.status(500).json(err)
-//     }
-// });
+    try {
+        let products;
 
+        if (qNew) {
+            products = await Product.find().sort({ createdAt: -1 }).limit(1);
+        } else if (qCategory) {
+            products = await Product.find({
+                categories: {
+                    $in: [qCategory],
+                },
+            });
+        } else {
+            products = await Product.find();
+        }
 
-// //GET USERs STATS 
-// router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
-//     const date = new Date();
-//     const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
-
-//     try {
-//         const data = await User.aggregate([
-//             { $match: { createdAt: { $gte: lastYear } } },
-
-//             {
-//                 $project: {
-//                     month: { $month: "$createdAt" },
-//                 },
-//             },
-        
-//             {
-//                 $group: {
-//                     _id: "$month",
-//                     total: { $sum: 1 },
-//                 }
-//             }
-
-//         ]);
-//         res.status(200).json(data);
-//     } catch (err) {
-//         res.status(500).json(err)
-//     }
-// })
+        res.status(200).json(products);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
 
 
 module.exports = router
