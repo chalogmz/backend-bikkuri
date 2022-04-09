0d91ce282c09e3976eb81f0fab9cd5b6aaf1e48e
Salome Gomez <sgomez713@misena.edu.co>
2022-04-08 21:10:43 -0500

    se creo logica para editar y actulizar orden

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -15,22 +15,23 @@ router.post("/", verifyToken, async (req,res) => {
     }
 });
 
-// //UPDATE
-// router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
 
-//     try {
-//         const updatedCart = await Cart.findByIdAndUpdate(
-//             req.params.id,
-//             {
-//                 $set: req.body,
-//             },
-//             { new: true }
-//         );
-//         res.status(200).json(updatedCart);
-//     }   catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
+//UPDATE
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+
+    try {
+        const updatedOrder = await Order.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: req.body,
+            },
+            { new: true }
+        );
+        res.status(200).json(updatedOrder);
+    }   catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 // //DELETE
