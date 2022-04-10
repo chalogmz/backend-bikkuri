fdbebda4576a43f6964303623158b27e02893036
Salome Gomez <sgomez713@misena.edu.co>
2022-04-10 17:33:44 -0500

    se creo la logica desde el lado del servidor para realizar los pagos usando stripe

---
+++ routes/stripe.routes.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const stripe = require("stripe")(process.env.STRIPE_KEY)
+
+
+router.post("/payment", (req, res, next) => {
+    stripe.charges.create({
+        source: req.body.tokenId,
+        amount: req.body.amount,
+        currency: "usd",
+    });
+});
+
+module.exports = router
